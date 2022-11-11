8e440c6e72263d609324288c192ada37b11599a5
lonevox <quinn.epicness@gmail.com>
2022-11-11 14:21:57 -0800

    Only allow TileBody to have one TileMap

--- addons/DestructibleTileMap/TileBody.cs
+++ addons/DestructibleTileMap/TileBody.cs
@@ -6,7 +6,7 @@ using System.Linq;
 [Tool]
 public partial class TileBody : RigidBody2D
 {
-    private HashSet<TileMap> tileMaps = new HashSet<TileMap>();
+    private TileMap tileMap;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -30,28 +30,26 @@ public partial class TileBody : RigidBody2D
         // BUG For some reason the below Remove doesnt work. It seems like the warning is added after this method is called.
         warnings.Remove("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape2D or CollisionPolygon2D as a child to define its shape.");
 
-        var tileMapChildren = GetChildren()
+        var tileMapChild = GetChildren()
             .Where(child => child is TileMap)
-            .Select(tileMap => tileMap as TileMap);
+            .Select(tileMap => tileMap as TileMap)
+            .FirstOrDefault();
         // Add warning for not having a TileMap child.
-        if (!tileMapChildren.Any())
+        if (tileMapChild == null)
         {
             warnings.Add("This node has no TileMap, so it can't be properly physically simulated. Without a TileMap, a TileBody acts the same as a RigidBody2D. Consider adding a TileMap as a child.");
         }
         else
         {
-            // Add warning for all TileMaps that don't have the required layers in their TileSet.
-            foreach (var tileMapChild in tileMapChildren)
+            // Add warning if the TileMap doesn't have the required layers in it's TileSet.
+            var tileSet = tileMapChild.TileSet;
+            if (tileSet?.GetCustomDataLayerByName("Mass") == -1)
             {
-                var tileSet = tileMapChild.TileSet;
-                if (tileSet?.GetCustomDataLayerByName("Mass") == -1)
-                {
-                    warnings.Add("Child TileMap '" + tileMapChild.Name + "' needs a custom layer called 'Mass' in it's TileSet to function correctly.");
-                }
-                if (tileSet?.GetCustomDataLayerByName("Strength") == -1)
-                {
-                    warnings.Add("Child TileMap '" + tileMapChild.Name + "' needs a custom layer called 'Strength' in it's TileSet to function correctly.");
-                }
+                warnings.Add("Child TileMap '" + tileMapChild.Name + "' needs a custom layer called 'Mass' in it's TileSet to function correctly.");
+            }
+            if (tileSet?.GetCustomDataLayerByName("Strength") == -1)
+            {
+                warnings.Add("Child TileMap '" + tileMapChild.Name + "' needs a custom layer called 'Strength' in it's TileSet to function correctly.");
             }
         }
 
@@ -61,19 +59,19 @@ public partial class TileBody : RigidBody2D
 
     private void OnChildEnteredTree(Node node)
     {
-        // Record new TileMap.
-        if (node is TileMap)
+        // Record TileMap.
+        if (tileMap == null && node is TileMap)
         {
-            tileMaps.Add(node as TileMap);
+            tileMap = node as TileMap;
         }
     }
 
     private void OnChildExitingTree(Node node)
     {
-        // Remove TileMap from tileMaps.
-        if (node is TileMap)
+        // Remove tileMap if this child was the TileMap.
+        if (node != null && node == tileMap)
         {
-            tileMaps.Remove(node as TileMap);
+            tileMap = null;
         }
     }
 }
